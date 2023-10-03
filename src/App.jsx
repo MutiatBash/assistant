17f2d7471bad67431367e0dcfd9e6e68f7a40b85
Bashua Mutiat <mutiatbashua@gmail.com>
2023-10-03 08:53:10 +0100

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>
+          <input />
+        </div>
+        <button>blue</button>
+        <button>green</button>
+        <button>pink</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
